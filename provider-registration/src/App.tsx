efb494a4b16d2576f1e6852880e37965fa13fc07
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-14 10:26:45 -0600

    Created form and fields.

--- provider-registration/src/App.tsx
+++ provider-registration/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import InputForm from './components/InputForm';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World!
+      <span className='heading'>Availity Registration</span>
+      <div>
+        <h2>
+          Welcome to Availity Provider Registration
+        </h2>
+      </div>
+      <div>
+        <InputForm />
+      </div>
     </div>
   );
 }
---
+++ provider-registration/src/components/InputForm.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const InputForm = () => {
+    return (
+        <form action="submit">
+            <input type="text" placeholder='First Name' className='input' />
+            <input type="text" placeholder='Last Name' className='input' />
+            <input type="text" placeholder='NPI Number' className='input' />
+            <input type="address" placeholder='Business Address' className='input' />
+            <input type="phone" placeholder='Phone Number' className='input' />
+            <input type="email" placeholder='Email Address' className='input' />
+            <button type='submit' className=''>Submit</button>
+        </form>
+    )
+}
+
+export default InputForm
