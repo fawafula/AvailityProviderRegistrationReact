910844c21563c0b6d6ece5d9ff711c9634913b01
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-14 16:05:07 -0600

    refactored app.jsx file and input form elements.

--- provider-registration/src/App.jsx
+++ provider-registration/src/App.jsx
@@ -5,21 +5,80 @@ import { FormInput } from './components/FormInput';
 
 const App = () => {
 
-  // const [firstName, setFirstName] = useState("")
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    npiNumber: "",
+    businessAddress: "",
+    phoneNumber: "",
+    emailAddress: ""
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "firstName",
+      type: "text",
+      placeholder: "First Name",
+      label: "First Name"
+    },
+    {
+      id: 2,
+      name: "lasttName",
+      type: "text",
+      placeholder: "Last Name",
+      label: "Last Name"
+    },
+    {
+      id: 3,
+      name: "npiNumber",
+      type: "text",
+      placeholder: "NPI Number",
+      label: "NPI Number"
+    },
+    {
+      id: 4,
+      name: "businessAddress",
+      type: "address",
+      placeholder: "Business Address",
+      label: "Bussiness Address"
+    },
+    {
+      id: 5,
+      name: "phoneNumber",
+      type: "phone",
+      placeholder: "Phone Number",
+      label: "Phone Number"
+    },
+    {
+      id: 6,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      label: "Email"
+    }
+  ]
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target);
+  }
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
   }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <FormInput name="firstName" placeholder="First Name" />
-        <FormInput name="lastName" placeholder="Last Name" />
-        <FormInput name="npiNumber" placeholder="NPI Number" />
-        <FormInput name="buisnessAddress" placeholder="Business Address" />
-        <FormInput name="phoneNumber" placeholder="Phone Number" />
-        <FormInput name="emailAddress" placeholder="Email Address" />
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+
         <button>Submit</button>
       </form>
     </div>
--- provider-registration/src/components/FormInput.jsx
+++ provider-registration/src/components/FormInput.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import "./formInput.css";
 
 export const FormInput = (props) => {
+    const { label, onChange, id, ...inputProps } = props;
     return (
         <div className='formInput'>
-            {/* <label htmlFor=""></label> */}
-            <input name={props.name} placeholder={props.placeholder} />
+            <label htmlFor="">{label}</label>
+            <input {...inputProps} onChange={onChange} />
         </div>
     )
 }
