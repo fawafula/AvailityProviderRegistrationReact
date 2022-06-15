3cbbe38317495342ad512fe2a227e3518333c134
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-15 06:42:09 -0600

    Added table

--- provider-registration/src/App.js
+++ provider-registration/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>NPI Number</th>
+            <th>Business Address</th>
+            <th>Phone Number</th>
+            <th>Email Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jon</td>
+            <td>Doe</td>
+            <td>123456789</td>
+            <td>123 Main St. Denver CO</td>
+            <td>123-456-2345</td>
+            <td>doe.jon@somemail.com</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
