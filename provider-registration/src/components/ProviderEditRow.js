341eaf8d5bbe2335ce4f392799fd05741679bb07
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-15 14:01:30 -0600

    Adjusted edit row input css.

--- provider-registration/src/components/ProviderEditRow.js
+++ provider-registration/src/components/ProviderEditRow.js
@@ -5,22 +5,22 @@ const ProviderEditRow = ({editFormData, handleDataEditChange, handleCancelClick}
   return (
     <tr className='tableRow'>
         <td>
-            <input type="text" name="firstName" value={editFormData.firstName} required="required" placeholder="First Name" onChange={handleDataEditChange}/>
+            <input className="input" type="text" name="firstName" value={editFormData.firstName} required="required" placeholder="First Name" onChange={handleDataEditChange}/>
         </td>
         <td>
-            <input type="text" name="lastName" value={editFormData.lastName} required="required" placeholder="Last Name" onChange={handleDataEditChange}/>
+            <input className="input" type="text" name="lastName" value={editFormData.lastName} required="required" placeholder="Last Name" onChange={handleDataEditChange}/>
         </td>
         <td>
-            <input type="text" name="npiNumber" value={editFormData.npiNumber} required="required" placeholder="NPI Number" onChange={handleDataEditChange}/>
+            <input className="input" type="text" name="npiNumber" value={editFormData.npiNumber} required="required" placeholder="NPI Number" onChange={handleDataEditChange}/>
         </td>
         <td>
-            <input type="address" name="businessAddress" value={editFormData.businessAddress} required="required" placeholder="Business Address" onChange={handleDataEditChange}/>
+            <input className="input" type="address" name="businessAddress" value={editFormData.businessAddress} required="required" placeholder="Business Address" onChange={handleDataEditChange}/>
         </td>
         <td>
-            <input type="phone" name="phoneNumber" value={editFormData.phoneNumber} required="required" placeholder="Phone Number" onChange={handleDataEditChange}/>
+            <input className="input" type="phone" name="phoneNumber" value={editFormData.phoneNumber} required="required" placeholder="Phone Number" onChange={handleDataEditChange}/>
         </td>
         <td>
-            <input type="email" name="email" value={editFormData.email} required="required" placeholder="Email" onChange={handleDataEditChange}/>
+            <input className="input" type="email" name="email" value={editFormData.email} required="required" placeholder="Email" onChange={handleDataEditChange}/>
         </td>
         <td>
             <button type="submit">Save</button>
