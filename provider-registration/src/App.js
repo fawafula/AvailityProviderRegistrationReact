5a2eb48bf4c96d39d51680a7a867d92a5863b97f
Felix Wafula <felix.a.wafula@gmail.com>
2022-06-15 07:29:30 -0600

    Added submit button functionality.

--- provider-registration/src/App.js
+++ provider-registration/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from "react";
+import { nanoid } from 'nanoid';
 import './App.css';
 import data from "./mock-data.json";
 
@@ -27,6 +28,23 @@ const App = () => {
 
     setAddFormData(newFormData);
 
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newProvider = {
+      id: nanoid(),
+      firstName: addFormData.firstName,
+      lastName: addFormData.lastName,
+      npiNumber: addFormData.npiNumber,
+      businessAddress: addFormData.businessAddress,
+      phoneNumber: addFormData.phoneNumber,
+      email: addFormData.email
+    }
+
+    const newProviders = [...providers, newProvider];
+    setProviders(newProviders);
   }
 
   return (
@@ -58,7 +76,7 @@ const App = () => {
       </table>
       
       <h2>Register Provider</h2>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <input type= "text" name="firstName" required="required" placeholder="First Name" onChange={handleDataChange}/>
         <input type= "text" name="lastName" required="required" placeholder="Last Name" onChange={handleDataChange}/>
         <input type= "text" name="npiNumber" required="required" placeholder="NPI Number" onChange={handleDataChange}/>
